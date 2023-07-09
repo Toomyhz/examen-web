df6b135ea98da38ec2dc987cfe947eaca6a1dbc7
Fab <fab.parraguez@duocuc.cl>
2023-07-09 04:52:55 -0400

    Se agrego crear productos, falta la funcion de eliminar y editar, estan hechas pero no las voy a implementar en este commit todavía por falta de diseño

--- apps/m_admin/urls.py
+++ apps/m_admin/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import listar_adm
+from .views import listar_adm, crear_producto
 
 urlpatterns = [
-    path('', listar_adm, name='listar_adm')
+    path('', listar_adm, name='listar_adm'),
+    path('crearp/', crear_producto, name='crear_producto')
 ]
--- apps/m_admin/views.py
+++ apps/m_admin/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import FormularioAdmProduct
+from django.contrib.auth.decorators import login_required
 from apps.m_tienda.models import Producto
 
 # Create your views here.
@@ -10,3 +11,16 @@ def listar_adm(request):
         'productos':productos
     }
     return render(request, 'pages/m_admin/listar.html', context)
+
+
+def crear_producto(request):
+    if request.method == 'POST':
+        formulario = FormularioAdmProduct(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            print('Producto añadido')
+            return redirect('crear_producto')
+    else:
+        formulario = FormularioAdmProduct()
+    context = {'form': formulario}
+    return render(request, 'pages/m_admin/crearp.html', context)
