c319009d12bb160feb1efbe551d190cde09d7e90
Fab <fab.parraguez@duocuc.cl>
2023-07-10 18:20:31 -0400

    Commit final (previsorio, quizás en un futuro se cambie)

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
